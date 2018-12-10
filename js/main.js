8e367a672d4c2f8b95a35f6e3db802ce55fcec2d
unknown <colpeetermans@gmail.com>
2018-12-10 15:58:11 +0100

    burger js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,19 @@
-$(".onglet-f-title").click(function() {
-    $(this).children(".fa-arrow-down").toggleClass("active");
-    $(this).next("p").slideToggle();
-    $(this).next("p").toggleClass("hidden");
+// menu burger
+
+$(".burger").click(function() {
+    $(this).toggleClass("burger-active"); //burger devient croix
+    $(this).next(".menu-burger").toggleClass("menu-active"); //menu s'affiche
 })
 
-// $(".onglet-f-title").click(function() {
-//     $(this).children(".fa-arrow-down").toggleClass("active");
-//     $(this).next("p").slideToggle();
-//     $(this).next("p").toggleClass("hidden-else");
-//     if ($(this).next("p").hasClass("hidden-else")) {
-//         $(this).next("p").slideDown();
-//         $(this).next("p").addClass("hidden-else");
-//     }
-//     else 
-//         $(this).next("p").slideUp();
-//         $(this).next("p").removeClass("hidden-else");
-// })
+// scroll burger
 
-//clic sur onglet title
-// toggle le rotate sur l'icone
-// toggle le slide du texte
+let scroll = $('main').offset().top; // calcule ce qu'il y a de top au dessus du main
 
-
-    //si clic sur un onglet title
-    //toggleClass sur l'icone 
-    //slideToggle sur le texte
-    //sinon
-    //removeCLass sur l'icone
-    //slideUp sur le txt
-
-    
+$(window).scroll(function() {
+    let scrollTop = $(window).scrollTop();  // calcule la distance déjà parcourue
+    if (scrollTop > scroll) { 
+        $('.burger').addClass('scroll-burger'); 
+    } else {
+        $('.burger').removeClass('scroll-burger'); 
+    }
+});
