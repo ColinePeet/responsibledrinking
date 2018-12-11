99c2f3d05173b18409b2f98248a7fc86c84fd915
unknown <colpeetermans@gmail.com>
2018-12-11 13:25:57 +0100

    js done

--- js/main.js
+++ js/main.js
@@ -3,7 +3,20 @@
 $(".burger").click(function() {
     $(this).toggleClass("burger-active"); //burger devient croix
     $(this).next(".menu-burger").toggleClass("menu-active"); //menu s'affiche
-})
+    
+//-------changement de couleur dans le menu selon où on est dans la page--------/
+    $(".menu-burger>a").each(function () {
+        let targetSection = $(this.hash);
+        let sectionOffset = targetSection.offset().top;
+        let scrollTop = $(window).scrollTop();
+        if(scrollTop >= sectionOffset-3) {
+            $(".menu-burger>a").each(function () {
+                $(this).removeClass("blue");
+            });
+            $(this).addClass("blue");
+        }
+    });
+});
 
 // scroll burger
 
@@ -17,3 +30,20 @@ $(window).scroll(function() {
         $('.burger').removeClass('scroll-burger'); 
     }
 });
+
+//----------scroll on link--------//
+
+$(".menu-burger>a").on('click', function(event) {
+    //---cache le menu au click-------//
+    $(".menu-burger").removeClass("menu-active");
+    $(".burger").removeClass("burger-active")
+    //-------smooth scroll---//
+    event.preventDefault();
+    let hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+    });
+});
+
